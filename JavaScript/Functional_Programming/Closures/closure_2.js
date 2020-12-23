7e659210ef551a8cbf783354697a57434ffe8e8d
Vladimir <vladimir160933@gmail.com>
2020-12-23 13:57:57 +0300

    added new feature

--- JavaScript/Functional_Programming/Closures/closure_2.js
+++ JavaScript/Functional_Programming/Closures/closure_2.js
@@ -1,3 +1,5 @@
+console.log('-------------------------------------------------------');
+
 const createCounter = () => {
 	let count = 0
 
@@ -21,3 +23,44 @@ counter.decrement();
 console.log(counter.get());
 
 // output -1
+
+
+console.log('-------------------------------------------------------');
+
+
+/*
+
+// Bad pracrice (without closure) because 'a' in the global scope
+
+
+let a = 0;
+
+function count () {
+	a++;
+	console.log(a);
+}
+
+count();
+count();
+count();
+
+*/
+
+function foo () {
+	let a = 0; // exist only inside foo
+	return function () { // closed on externl funciton closure
+		a++;
+		console.log(a);
+	}
+}
+
+// closure = cod of the function + data to the function 
+let d = foo();
+
+let a = 'Hello'; // won't affect animore
+
+d();
+d();
+d();
+
+console.log('-------------------------------------------------------');
