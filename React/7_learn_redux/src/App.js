bed37c67d6842f74e0e458124e06862ad00ebf3a
Vladimir <vladimir160933@gmail.com>
2021-02-25 17:43:05 +0300

    react-redux

--- React/7_learn_redux/src/App.js
+++ React/7_learn_redux/src/App.js
@@ -1,21 +1,36 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from './Actions';
+import Test from './Components/Test';
+
+import './App.css';
 
 function App() {
 
-	const counter = useSelector(state => state.counter);
-	const isLogged = useSelector(state => state.isLogged);
-	const dispatch = useDispatch();
+  const increment = () => {
+    return {
+      type: 'INCREMENT'
+    };
+  };
+
+  const decrement = () => {
+    return {
+      type: 'DECREMENT'
+    };
+  };
+
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
-      <h1>Hello {counter} </h1>
+      <h1>Hello {counter}</h1>
 
-      <button onClick={() => dispatch(increment(5))} >+</button>
-      <button onClick={() => dispatch(decrement())} >-</button>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
 
       { isLogged ? <h2>Secret information</h2> : 'You are not logged in' }
-      
+
+      <Test />
     </div>
   );
 }
--- React/7_learn_redux/src/index.js
+++ React/7_learn_redux/src/index.js
@@ -1,20 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import allReducer from './Reducers';
 
-const store = createStore(allReducer);
+const counterReducer = (state = 0, action) => {
+	switch (action.type) {
+		case 'INCREMENT':
+			return state + 1;
+		case 'DECREMENT': 
+			return state - 1;
+		default:
+			return state;
+	};
+};
 
+const loggedReducer = (state = false, action) => {
+	switch (action.type) {
+		case 'SIGN_IN':
+			return !state;
+		default:
+			return state;
+	}
+};
 
+const textTest = () => {
+	return 'bla bla 123';
+};
+
+const allReducers = combineReducers({
+	counter: counterReducer,
+	isLogged: loggedReducer,
+	text: textTest
+});
+
+const store = createStore(allReducers);
+ 
 ReactDOM.render(
-	<React.StrictMode>
-		<Provider store={store} >
-	    	<App />
-  		</Provider>
-	</React.StrictMode>,
+  <React.StrictMode>
+  	<Provider store={store}> 
+    	<App />
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
